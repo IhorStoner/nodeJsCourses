d2c6a28dd76e4651d92ae16e12485709ff953a7b
IhorStoner <60327455+IhorStoner@users.noreply.github.com>
2021-02-22 22:48:55 +0200

    initial

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
 const { processEnv } = require("./lib/process.env");
-const { processArgv } = require("./lib/multiply");
-require("./lib/division");
-require("./lib/patch.console");
-
-const A = 10;
-const B = 2;
-
-const result1 = add(A, B);
-const result2 = multiply(A, B);
-const result3 = division(A, B);
-
-console.log("add", result1);
-console.log("multiply", result2);
-console.log("division", result3);
+const { processArgv } = require("./lib/process.argv");
